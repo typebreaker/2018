56d0eb30b5848976d6b99a89fbb00559454ee6eb
typebreaker <typebreaker@DESKTOP-1GGH33F>
2018-07-25 21:11:15 +0900

    calculating words and sorting

--- SortWords/src/main/java/kr/typebreaker/main.java
+++ SortWords/src/main/java/kr/typebreaker/main.java
@@ -8,8 +8,15 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class main {
 
@@ -41,16 +48,41 @@ public class main {
 		total=total.replaceAll("에게", "");
 		total=total.replaceAll("이라서", "");
 		String[] words = total.split(" ");
-	
+		
 		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("D:/words.txt"));
 	
 		for(int i=0;i<words.length;i++) {
-			words[i]+="\r\n";
-			bos.write(words[i].getBytes());
+			if(hash.get(words[i])==null)
+				hash.put(words[i], 0);
+			else
+				hash.put(words[i], hash.get(words[i])+1);
 		}
+		
+		Iterator itor = sort(hash).iterator();
+		while(itor.hasNext()) {
+			String nowSentense = (String) itor.next();
+			String putSentense=nowSentense+" "+hash.get(nowSentense)+"\r\n";
+			bos.write(putSentense.getBytes());
+		}
+		
 		bos.flush();
 		bos.close();
 		
+	}
+	
+	public static List sort(final Map map) {
+		List<String> list = new ArrayList();
+		list.addAll(map.keySet());
+		Collections.sort(list, new Comparator() {
+
+			@Override
+			public int compare(Object arg0, Object arg1) {
+				Object v1 = map.get(arg0);
+				Object v2 = map.get(arg1);
+				return ((Comparable)v2).compareTo(v1);
+			}
+		});
+		return list;
 	}
 
 }
